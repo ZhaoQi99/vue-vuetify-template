9f6862158a15e4817cfc032bc7a603d23a53026b
Qi Zhao <zhaoqi99@outlook.com>
2020-01-16 22:02:12 +0800

    fix: 🐛 avatar show

--- my-template/src/components/ToolBar.vue
+++ my-template/src/components/ToolBar.vue
@@ -15,26 +15,20 @@
         <v-icon>mdi-bell</v-icon>
       </v-badge>
     </v-btn>
-    <v-menu
-      offset-y
-      origin="center center"
-      :nudge-bottom="10"
-      transition="scale-transition"
-    >
+    <v-menu offset-y origin="center center" :nudge-bottom="10" transition="scale-transition">
       <template v-slot:activator="{ on }">
         <v-btn icon v-on="on">
-          <v-avatar size="40px">
+          <v-avatar v-if="avatar" size="40px">
             <v-img :src="avatar"></v-img>
+          </v-avatar>
+          <v-avatar v-else color="red" size="40px">
+            <span class="white--text headline">{{ username }}</span>
           </v-avatar>
         </v-btn>
       </template>
       <v-list flat>
         <v-list-item-group>
-          <v-list-item
-            v-for="(item, index) in items"
-            @click="item.click"
-            :key="index"
-          >
+          <v-list-item v-for="(item, index) in items" @click="item.click" :key="index">
             <v-list-item-icon>
               <v-icon v-text="item.icon"></v-icon>
             </v-list-item-icon>
@@ -52,7 +46,6 @@ export default {
   name: "Toolbar",
   data() {
     return {
-      avatar: "https://cdn.vuetifyjs.com/images/john.jpg",
       notificationsCount: 1, // change to 0 in the future
       items: [
         {
@@ -83,6 +76,18 @@ export default {
     handleProfile() {},
     changeDrawer() {
       this.$emit("changeDrawer");
+    }
+  },
+  computed: {
+    avatar() {
+      if (localStorage.avatar === undefined || localStorage.avatar === "null") {
+        return undefined;
+      } else {
+        return localStorage.avatar;
+      }
+    },
+    username() {
+      return (localStorage.username || "??").slice(0, 2);
     }
   }
 };
--- my-template/vue.config.js
+++ my-template/vue.config.js
@@ -3,6 +3,9 @@ module.exports = {
     proxy: {
       "/api": {
         target: "http://127.0.0.1:8000"
+      },
+      "/media": {
+        target: "http://127.0.0.1:8000"
       }
     },
     disableHostCheck: true
